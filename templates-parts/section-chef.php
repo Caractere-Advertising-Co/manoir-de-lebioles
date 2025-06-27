d7a84cedbb932793e67dd15a934c34cb5a861e49
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-06-27 10:22:39 +0200

    add cta section chef - spa

--- templates-parts/section-chef.php
+++ templates-parts/section-chef.php
@@ -15,8 +15,17 @@ $ctaChef        = get_field('cta-chef','options');
     </div>
   <?php endif;?>
   <div id="text">
-    <div class="text">
-      <?php if($txtChef): echo $txtChef; endif;?>
+    <div class="text container columns">
+      <div class="colg">
+        <?php if($txtChef): echo $txtChef; endif;?>
+      </div>
+      <div class="cold">
+        <?php if($ctaChef):?>
+          <a class="cta -border" href="<?php echo $ctaChef['url'];?>">
+            <?php echo $ctaChef['title'];?>
+          </a>
+        <?php endif;?>
+      </div>
     </div>
   </div>
 </section>
