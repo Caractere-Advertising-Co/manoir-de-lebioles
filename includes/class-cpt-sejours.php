49dc7579cdebb2dae818f75f266cf4bee72300de
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-07-03 15:43:23 +0200

    add class-cpt-sejours - + singlesejours

---
+++ includes/class-cpt-sejours.php
@@ -0,0 +1,37 @@
+<?php 
+
+class CPT_Sejours {
+    public static function register() {
+        $labels = array(
+            'name' => _x('Séjour.s', 'Post Type General Name', 'custom_post_type'),
+            'singular_name' => _x('Séjour', 'Post Type Singular Name', 'custom_post_type'),
+            'menu_name' => __('Séjours', 'custom_post_type'),
+            'name_admin_bar' => __('Séjours', 'custom_post_type'),
+            'add_new' => __('Ajouter séjour', 'custom_post_type'),
+            'add_new_item' => __('Ajouter une nouvelle séjour', 'custom_post_type'),
+            'new_item' => __('Nouveau', 'custom_post_type'),
+            'edit_item' => __('Modifier', 'custom_post_type'),
+            'view_item' => __('Voir', 'custom_post_type'),
+            'all_items' => __('Toutes les séjours', 'custom_post_type'),
+            'search_items' => __('Recherche', 'custom_post_type'),
+            'not_found' => __('Non trouvé', 'custom_post_type'),
+        );
+
+        $args = array(
+            'label' => __('Séjours', 'custom_post_type'),
+            'description' => __('Galerie séjour', 'custom_post_type'),
+            'labels' => $labels,
+            'public' => true,
+            'has_archive' => true,
+            'menu_position' => 4,
+            'menu_icon' => 'dashicons-schedule',
+            'supports' => array('title', 'revisions', 'author', 'thumbnail'),
+        );
+
+        register_post_type('sejours', $args);
+    }
+}
+
+add_action('init', ['CPT_Séjours', 'register']);
+
+?>
---
+++ single-sejours.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Template Name: Single Séjours
+*/
+
+$cta_form    = get_field('formulaire_postuler');
+$cta_contact = get_field('cta_contact');
+
+get_header();
+
+get_template_part('templates-parts/section-hero');
+?>
+
+<section id="description-chambre">
+    <div class="container">
+        <?php the_content();?>
+        
+        <div class="columns block-cta">
+                <a href="<?php echo $cta_form  ['url'];?>" class="cta"><?php echo $cta_form  ['title'];?></a>
+                <a href="<?php echo $cta_contact['url'];?>" class="cta-white"><?php echo $cta_contact['title'];?></a>
+        </div>
+    </div>
+</section>
+
+<main id="acf-modulable-content" class="site-content">
+  <?php get_template_part( 'templates-parts/block-builder' );?>
+</main>
+
+
+<?php get_template_part('templates-parts/section-logo');?>
+<?php get_template_part('templates-parts/section-newsletter');?>
+<?php get_footer(); ?>
