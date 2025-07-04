46d274815f29b5ed65081e85c4a3b3050c763f0b
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-07-04 13:49:08 +0200

    front-page + section-table-sejours

--- templates-parts/builder/section-table-sejours.php
+++ templates-parts/builder/section-table-sejours.php
@@ -15,21 +15,24 @@ $query = new WP_Query($args);
             $i = 0;
             while ($query->have_posts()): $query->the_post(); // fixed: was the_posts()
                 $galerie = get_field('galerie-hero');
-                $bg = $galerie[0]['hero']['url'] ?? null; // added null coalescing in case it's empty
+                $bg = $galerie[0]['hero']['url']; // added null coalescing in case it's empty
+                $bgTitle = $galerie[0]['hero']['title'];
 
                 $titre = get_the_title();
                 $lien  = get_permalink();
                 ?>
 
-                <div class="col from-bottom <?php echo ($i % 2 != 0) ? '-slow' : ''; ?>">
-                    <div class="block-img">
-                        <?php if ($bg): ?>
-                            <img src="<?php echo esc_url($bg['url']); ?>" alt="<?php echo esc_attr($bg['title']); ?>"/>
-                        <?php endif; ?>
+                    <div class="col from-bottom <?php echo ($i % 2 != 0) ? '-slow' : ''; ?>">
+                        <a href="<?php echo esc_url($lien); ?>">
+                            <div class="block-img">
+                                <?php if($bg): ?>
+                                    <img src="<?php echo esc_url($bg); ?>" alt="<?php echo esc_attr($bgTitle); ?>"/>
+                                <?php endif; ?>
+                            </div>
+                            <h4><?php echo $titre; ?></h4>
+                            <span class="cta-line">En savoir plus</span>
+                        </a>
                     </div>
-                    <h4><?php echo esc_html($titre); ?></h4>
-                    <a href="<?php echo esc_url($lien); ?>" class="cta-line">En savoir plus</a>
-                </div>
                 <?php $i++;
             endwhile;
             wp_reset_postdata();
