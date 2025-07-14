3049535b6c7293d8ab0ec704676e15630a6b8003
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-07-14 14:25:34 +0200

    single.pgp

--- builder.php
+++ builder.php
@@ -1,24 +1,10 @@
 <?php
 /*
 Template Name: Page Builder
-Description: Page vierge avec Elementor
 */
-?>
-
-<?php
-// Elementor preview mode check
-if ( isset($_GET['elementor-preview']) && $_GET['elementor-preview'] === 'true' ) {
-    define( 'ELEMENTOR_PREVIEW', true );
-}
 
 get_header();
 get_template_part('templates-parts/section-hero');?>
-
-<main id="elementor-content" class="site-content">
-  <?php while ( have_posts() ) : the_post();
-    the_content();
-  endwhile;?>
-</main>
 
 <main id="acf-modulable-content" class="site-content">
   <?php get_template_part( 'templates-parts/block-builder' );?>
--- single.php
+++ single.php
@@ -8,21 +8,7 @@ get_header();
 get_template_part('templates-parts/section-hero');
 ?>
 
-<section id="description-chambre">
-    <div class="container">
-        <?php the_content();?>
-        
-        <div class="columns block-cta">
-                <a href="<?php echo $cta_form  ['url'];?>" class="cta"><?php echo $cta_form  ['title'];?></a>
-                <a href="<?php echo $cta_contact['url'];?>" class="cta-white"><?php echo $cta_contact['title'];?></a>
-        </div>
-    </div>
-</section>
-
-<main id="acf-modulable-content" class="site-content">
-  <?php get_template_part( 'templates-parts/block-builder' );?>
-</main>
-
+<?php get_template_part( 'templates-parts/block-builder' );?>
 
 <?php get_template_part('templates-parts/section-logo');?>
 <?php get_template_part('templates-parts/section-newsletter');?>
