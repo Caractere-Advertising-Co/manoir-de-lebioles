085d2a685eb036b31fbcf998b6ff7a754b3b98e9
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-07-14 11:11:15 +0200

    cta actualités

--- templates-parts/section-actualites.php
+++ templates-parts/section-actualites.php
@@ -26,13 +26,15 @@
     <?php if($query):
         while($query->have_posts()): $query->the_post();?>
             <div class="card-actus">
-                <div class="block-img imgCard">
-                    <img src="<?php echo the_post_thumbnail_url(  );?>" alt="<?php echo the_post_thumbnail_caption( );?>" />
-                    <span class="date"><?php echo get_the_date("d.m.Y");?></span>
-                </div>
-                <h3 class="titleCard"><?php the_title();?></h3>
-                <?php the_excerpt( );?>
-                <a href="#" class="ctaCard">Lire plus</a>
+                <a href="<?php echo get_permalink();?>">
+                    <div class="block-img imgCard">
+                        <img src="<?php echo the_post_thumbnail_url(  );?>" alt="<?php echo the_post_thumbnail_caption( );?>" />
+                        <span class="date"><?php echo get_the_date("d.m.Y");?></span>
+                    </div>
+                    <h3 class="titleCard"><?php the_title();?></h3>
+                    <?php the_excerpt( );?>
+                    <p class="ctaCard">Lire plus</p>
+                </a>
             </div>
         <?php endwhile;
     endif;
